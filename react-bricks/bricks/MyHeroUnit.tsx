16d08d2e205ff4eb36ca14dd5e7ca830ae9d5256
Matteo Frana <f2@f2.net>
2021-10-11 18:24:15 +0200

    Changed Hero Unit maxWidth

--- react-bricks/bricks/MyHeroUnit.tsx
+++ react-bricks/bricks/MyHeroUnit.tsx
@@ -23,7 +23,7 @@ const MyHeroUnit: types.Brick<HeroUnitProps> = ({ padding }) => {
       }`}
     >
       <div className="flex flex-col items-center">
-        <Image propName="icon" alt="Icon" maxWidth={400} aspectRatio={1} imageClassName="w-20 mb-5" />
+        <Image propName="icon" alt="Icon" maxWidth={80} aspectRatio={1} imageClassName="w-20 mb-5" />
         <Text
           renderBlock={(props) => (
             <h1 className="text-3xl sm:text-4xl text-center font-black text-gray-900 dark:text-white leading-tight mb-3">
